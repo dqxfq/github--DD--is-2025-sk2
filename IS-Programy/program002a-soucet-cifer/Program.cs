4f4ec9a8ea992edea849d7e8d23a32dc694a2976
doubravka dostalova <doubravkadostalova7@gmail.com>
2025-10-22 23:14:49 +0200

    Vyřešen úkol program002a - součet a součin cifer. Closes #3

---
+++ IS-Programy/program002a-soucet-cifer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Zadej celé číslo: ");
+        int cislo = int.Parse(Console.ReadLine());
+
+        int soucet = 0;
+        int soucin = 1;
+        int n = Math.Abs(cislo);
+
+        if (n == 0)
+        {
+            soucet = 0;
+            soucin = 0;
+        }
+        else
+        {
+            while (n > 0)
+            {
+                int cifra = n % 10;
+                soucet += cifra;
+                soucin *= cifra;
+                n /= 10;
+            }
+        }
+
+        Console.WriteLine($"Součet číslic: {soucet}");
+        Console.WriteLine($"Součin číslic: {soucin}");
+    }
+}
