f49dd06867f4d06785cf0dfcda9bca1abaddd4df
Doubravka <doubravkadostalova7@gmail.com>
2025-12-11 13:44:38 +0100

    #14 program pro vytvoření hlavičky a razítka

---
+++ IS-Programy/program013a-NSD-NSN/Program.cs
@@ -0,0 +1,49 @@
+﻿﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+  //metoda pro výpis hlavičky/razítka
+
+razitko()
+ulong a = nactiCislo("Zadejte cislo A: ")
+ulong b = nactiCislo("Zadejte cislo B: ")
+ 
+
+
+
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+
+
+
+
+static void razitko() [
+    Console.Clear();
+    Console.WriteLine("*********************************************************");
+    Console.WriteLine("*********** Generátor pseudonáhodných čísel *************");
+    Console.WriteLine("*********************************************************");
+    Console.WriteLine("*********************************************************");
+    Console.WriteLine("************* Doubravka Dostálová ******************");
+    Console.WriteLine("************** 6.11.2025 *******************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine();
+]
+
+static ulong (string zprava)
+{
+
+     Console.Write("Zadejte první číslo řady (celé číslo): ");
+    int first;
+    while (!int.TryParse(Console.ReadLine(), out first))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu: ");
+    }
+    return cislo;
+}
+
+
+
+  }
