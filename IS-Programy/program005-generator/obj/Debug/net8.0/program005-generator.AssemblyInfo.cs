1ea39524f20fc37c7ef787699e3531d1f55d15fc
doubravka dostalova <doubravkadostalova7@gmail.com>
2026-01-05 19:17:46 +0100

    #7 pseudonáhodná čísla, kod 00b a program 005

--- IS-Programy/program005-generator/obj/Debug/net8.0/program005-generator.AssemblyInfo.cs
+++ IS-Programy/program005-generator/obj/Debug/net8.0/program005-generator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("program005-generator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1897ca478a3f18748423b7df799dbf0b9fa96e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e9142f7baa3d1882dadd7dd47e944d75ab0d7cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("program005-generator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("program005-generator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- IS-Programy/program00b/Program.cs
+++ IS-Programy/program00b/Program.cs
@@ -2,67 +2,68 @@
 while (again == "a")
 {
     Console.Clear();
-    Console.WriteLine("********************************************");
-    Console.WriteLine("**** Generátor pseoudonáhodných čísel ******");
-    Console.WriteLine("********************************************");
-    Console.WriteLine("********************************************");
-    Console.WriteLine("********** Doubravka Dostálová *************");
+    Console.WriteLine("*********************************************************");
+    Console.WriteLine("*********** Generátor pseudonáhodných čísel *************");
+    Console.WriteLine("*********************************************************");
+    Console.WriteLine("*********************************************************");
+    Console.WriteLine("************* Doubravka Dostálová ******************");
     Console.WriteLine("************** 6.11.2025 *******************");
     Console.WriteLine("********************************************");
     Console.WriteLine("********************************************");
     Console.WriteLine();
 
-    // Vstup hodnoty do programu, ale špatně řešený
-    //Console.Write("Zadejte první číslo řady: ");
-    //int first = int.Parse(Console.ReadLine());
-
-    // Vstup hodnoty do programu, řešený lépe
-    Console.Write("Zadejte počet předgenerovaných čísel: ");
-    int n;
-    while (!int.TryParse(Console.ReadLine(), out n))
+    Console.Write("Zadejte počet generovaných čísel (celé číslo): ");
+    int pocet;
+    while (!int.TryParse(Console.ReadLine(), out pocet))
     {
-        Console.Write("Nezadali jste celé číslo. Zadejte počet předgenerovaných čísel znovu: ");
+        Console.Write("Nezadali jste celé číslo. Zadejte počet čísel znovu: ");
     }
 
+
     Console.Write("Zadejte dolní mez (celé číslo): ");
-    int lowerBound; // Dolní mez
-    while (!int.TryParse(Console.ReadLine(), out lowerBound))
+    int dolnimez; // Dolní mez
+    while (!int.TryParse(Console.ReadLine(), out dolnimez))
     {
         Console.Write("Nezadali jste celé číslo. Zadejte dolní mez znovu: ");
     }
 
+
+
     Console.Write("Zadejte horní mez (celé číslo): ");
-    int upperBound; // Horní mez
-    while (!int.TryParse(Console.ReadLine(), out upperBound))
+    int hornimez; // Horní mez
+    while (!int.TryParse(Console.ReadLine(), out hornimez))
     {
         Console.Write("Nezadali jste celé číslo. Zadejte horní mez znovu: ");
     }
 
 
-
-
-
     Console.WriteLine();
-    Console.WriteLine("============================================");
-    Console.WriteLine("Zadané hodnoty: ");
-    Console.WriteLine("Počet čísel {0}, dolní mez: {1}, horní mez: {2}", n, lowerBound, upperBound);
-    Console.WriteLine("============================================");
+    Console.WriteLine("==========================================================");
+    Console.WriteLine("Zadané hodnoty:");
+    Console.WriteLine("Počet čísel: {0}, Dolní mez: {1}, Horní mez: {2}", pocet, dolnimez, hornimez);
+    Console.WriteLine("==========================================================");
     Console.WriteLine();
 
-    //deklarace pole (array)
-    int[] myRandnumbs = new int[n];
+    // deklarace pole (array)
+    int[] cisla = new int[pocet];
 
-    //příprava pro využitíní třídy random
-    Random myRandnumb = new Random();
-
+    // příprava pro využití třídy Random
+    Random generator = new Random();
+    //Random myRandNumb = new Random(15);
+    
     Console.WriteLine();
     Console.WriteLine("Náhodná čísla: ");
-    for (int i = 0; i < n; i++)
+    for(int i = 0; i < pocet ; i++)
     {
-        myRandnumbs[i] = myRandnumb.Next(lowerBound, upperBound);
-        Console.WriteLine(myRandnumbs[i]);
-    }
+        cisla[i] = generator.Next(dolnimez, hornimez+1);
+        Console.Write("{0}; ",cisla[i]);
+
+    }  
 
 
-    again = Console.ReadLine() ?? "";
+
+    Console.WriteLine();
+    Console.WriteLine("==========================================================");
+    again = Console.ReadLine();
+
 }
